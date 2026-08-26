ec2b6b7da13470f0e77f4d9ba8172708b72c8167
martynawieruszewska <martyna.wieruszewska@gmail.com>
2026-08-26 22:35:33 +0200

    validation functions

--- src/.ipynb_checkpoints/validation-checkpoint.py
+++ src/.ipynb_checkpoints/validation-checkpoint.py
@@ -1,3 +1,7 @@
+from pandas.api.types import is_numeric_dtype
+# --- Relationship Validation ---
+
+# Check foreign key integrity
 def check_foreign_keys(left_df, right_df, left_key, right_key):
     merged = left_df.merge(
         right_df,
@@ -10,3 +14,27 @@ def check_foreign_keys(left_df, right_df, left_key, right_key):
     orphan_count = (merged["_merge"] == "left_only").sum()
 
     return orphan_count
+
+# --- Duplicates ---
+
+# Function 1 — Full Row Duplicates
+def check_duplicates(df):
+     return df.duplicated().sum()
+
+# Function 2 — Primary Key Duplicates
+def check_key_duplicates(df, key):
+    return df[key].duplicated().sum()
+
+# --- Missing Values ---
+def check_missing_values(df):
+    return df.isna().sum()
+
+# --- Check amounts ---
+def check_amounts(df, column):
+    if not is_numeric_dtype(df[column]):
+        raise TypeError(f"Column '{column}' must be numeric.")
+
+    negative_count = (df[column] < 0).sum()
+    zero_count = (df[column] == 0).sum()
+
+    return negative_count, zero_count
