1718eb5d92c56057f8255fe2841fa384f3864a80
martynawieruszewska <martyna.wieruszewska@gmail.com>
2026-08-31 15:50:09 +0200

    merchant analysis init

---
+++ sql/analysis/02_merchant_analysis.sql
@@ -0,0 +1,10 @@
+/*
+===============================================================================
+FinSight - Merchant Category Analysis
+===============================================================================
+Purpose:
+    - Analyzes transaction activity across merchant categories.
+    - Identifies categories with the highest transaction volume and value.
+    - Compares average transaction value and fraud activity by merchant category.
+===============================================================================
+*/
